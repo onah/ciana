9edce752bae39618adb3a9192ed2499ce6d61a81
HANO Hiroyuki <onahdwahan@gmail.com>
2020-03-08 09:35:26 +0900

    Add SetUp() in test_ast_reader_libtooling.cc

--- test/ast_reader/test_ast_reader_libtooling.cc
+++ test/ast_reader/test_ast_reader_libtooling.cc
@@ -4,41 +4,65 @@
 
 #include <fstream>
 #include <iostream>
+#include <vector>
 
-TEST(get_used_locations_Test, normal) {
+class ASTReaderLibToolingTest : public ::testing::Test {
+ protected:
 
-  const std::string FileName = ".cianarc";
-  std::ofstream File;
-  File.open(FileName, std::ios::out);
-  File << "../../../test/ciana_test/c_valiable_function/build/"
-          "compile_commands.json"
-       << std::endl;
+  virtual void SetUp() {
+    const std::string FileName = ".cianarc";
+    std::ofstream File;
+    File.open(FileName, std::ios::out);
+    File << "../../../test/ciana_test/c_valiable_function/build/"
+            "compile_commands.json"
+         << std::endl;
+  }
+
+};
+
+TEST_F(ASTReaderLibToolingTest, getUsedLocationsNormal) {
 
   Ciana::ASTReaderLibTooling AstReader;
-
   ASSERT_NO_FATAL_FAILURE(ASSERT_TRUE(AstReader.initialize() == true));
-
+ 
   Ciana::Location Input(
-      "../../../test/ciana_test/c_valiable_function/src/multi.c", 10, 18);
-  std::vector<Ciana::Location> Output;
-  bool Result = AstReader.get_used_locations(Input, Output);
+      "../../../test/ciana_test/c_valiable_function/src/multi.c", 10, 17);
+  std::vector<Ciana::Location> Outputs;
+  bool Result = AstReader.get_used_locations(Input, Outputs);
   ASSERT_EQ(true, Result);
 
-  ASSERT_EQ(4, Output.size());
+  ASSERT_EQ(4, Outputs.size());
 
   // TODO; do test for filename
 
-  ASSERT_EQ(8, Output[0].get_line());
-  ASSERT_EQ(5, Output[0].get_column());
+  ASSERT_EQ(8, Outputs[0].get_line());
+  ASSERT_EQ(5, Outputs[0].get_column());
 
-  ASSERT_EQ(8, Output[1].get_line());
-  ASSERT_EQ(21, Output[1].get_column());
+  ASSERT_EQ(8, Outputs[1].get_line());
+  ASSERT_EQ(20, Outputs[1].get_column());
 
-  ASSERT_EQ(10, Output[2].get_line());
-  ASSERT_EQ(18, Output[2].get_column());
+  ASSERT_EQ(10, Outputs[2].get_line());
+  ASSERT_EQ(17, Outputs[2].get_column());
 
-  ASSERT_EQ(11, Output[3].get_line());
-  ASSERT_EQ(10, Output[3].get_column());
+  ASSERT_EQ(11, Outputs[3].get_line());
+  ASSERT_EQ(10, Outputs[3].get_column());
 
   return;
 }
+
+/* 
+TEST_F(ASTReaderLibToolingTest, get) {
+  Ciana::Location Input(
+      "../../../test/ciana_test/c_valiable_function/src/multi.c", 10, 17);
+  
+  std::vector<Ciana::Location> Outputs;
+  bool Result = AstReader.getImpactVariable(Input, Outputs);
+  ASSERT_EQ(true, Result);
+
+  ASSERT_EQ(1, Outputs.size());
+
+  // TODO: ASSERT_EQ("../../../test/ciana_test/c_valiable_function/src/multi.c", Outputs.get_filename);
+  ASSERT_EQ(10, Outputs[0].get_line());
+  ASSERT_EQ(3, Outputs[0].get_column());
+}
+*/
